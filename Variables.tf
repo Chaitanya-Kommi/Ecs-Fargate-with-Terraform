7aff5538695aae5a79412ae8945b94a5275353ad
Kommi Chaitanya (IND) <kommi.chaitanya@protivitiglobal.in>
2024-05-30 12:41:35 +0530

    added comments to the variables

--- Variables.tf
+++ Variables.tf
@@ -1,21 +1,28 @@
-/*variable "access_key" {
-  default = ""
+variable "access_key" {
+  default = "YOUR_AWS_ACCESS_KEY_ID"
 }
+
 variable "secret_key" {
-  default = ""
+  default = "YOUR_AWS_SECRET_ACCESS_KEY"
 }
+
 variable "region" {
   default = "ap-south-1"
 }
+
 variable "subnet1" {
-  default = ""
+  default = "YOUR_SUBNET_ID_1"
 }
+
 variable "subnet2" {
-  default = ""  
+  default = "YOUR_SUBNET_ID_2"
 }
+
 variable "sg" {
-  default = ""
+  default = "YOUR_SECURITY_GROUP_ID"
 }
+
 variable "vpc" {
-  default = ""
-}*/
+  default = "YOUR_VPC_ID"
+}
+
